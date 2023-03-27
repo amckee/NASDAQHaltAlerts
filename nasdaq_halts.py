e1b1616e2bba25270ebd750a76890d2bccf0af81
Adam <armware@gmail.com>
2023-03-27 14:15:20 -0500

    Do not show single halts

--- nasdaq_halts.py
+++ nasdaq_halts.py
@@ -17,7 +17,8 @@ most_halts = Counter(halts).most_common(3)
 stroutput = "Symbols that halted the most:\r\n"
 
 for halt in most_halts:
-    stroutput += ( "%s : %s" % (halt[0], halt[1]) )
-    stroutput += "\r\n"
+    if halt[1] > 1:
+        stroutput += ( "%s : %s" % (halt[0], halt[1]) )
+        stroutput += "\r\n"
 
 subprocess.run( ["notify-send", "-i", "./usd.svg", "-a", "NASDAQ Halts", stroutput])
