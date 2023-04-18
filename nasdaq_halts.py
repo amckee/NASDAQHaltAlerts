8eefa91060066baf2bfad49159c1118b07a7af53
Adam <armware@gmail.com>
2023-04-18 09:53:33 -0500

    Conditional improvements

--- nasdaq_halts.py
+++ nasdaq_halts.py
@@ -2,17 +2,19 @@
 
 import feedparser, dbus
 from collections import Counter
+import datetime
 
 url = "http://www.nasdaqtrader.com/rss.aspx?feed=tradehalts"
-
 feed = feedparser.parse( url )
 
 halts = []
 
 for entry in feed.entries:
-    halts.append( entry.ndaq_issuesymbol )
+    haltdate = datetime.datetime.strptime(entry.ndaq_haltdate, '%m/%d/%Y')
+    if haltdate.date() == datetime.datetime.today().date():
+        halts.append( entry.ndaq_issuesymbol )
 
-most_halts = Counter(halts).most_common(3)
+most_halts = Counter(halts).most_common(5)
 
 stroutput = "Symbols that halted the most:\r\n"
 
@@ -21,7 +23,7 @@ for halt in most_halts:
         stroutput += ( "%s : %s" % (halt[0], halt[1]) )
         stroutput += "\r\n"
 
-if stroutput != "Symbols that halted the most:\r\n":
+if len( most_halts ) > 0:
     obj = dbus.SessionBus().get_object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
     obj = dbus.Interface(obj, "org.freedesktop.Notifications")
-    obj.Notify("NASDAQ Halts", 0, "", "NASDAQ Halts", stroutput, [], {"urgency": 1}, 2000)
+    obj.Notify("NASDAQ Halts", 0, "", "NASDAQ Halts", stroutput, [], {"urgency": 1}, 5000)
